66d4b0b2492e7700ea29bb062ea98f46e6528829
colesec <177176161+colesec@users.noreply.github.com>
2026-02-13 16:13:47 +1000

    changed to class

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 let books = [];
 
-function Book(title, author, pages){
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.id = crypto.randomUUID();
+class Book{
+  constructor(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+  }
 }
 
-function addBookToLibrary(title, author, pages){
-  newBook = new Book(title, author, pages);
+function addBookToLibrary(title, author, pages, read){
+  newBook = new Book(title, author, pages, read);
   books.push(newBook);
 
   updateCards();
 }
 
-function removeBook(e){
-  let id = Array.from(e.target.parentElement.children)[3].textContent;
+function getBookIndex(e){
+  let id = Array.from(e.target.closest(".card").children)[4].textContent;
   for(let i=0; i<books.length; i++){
     if (books[i].id == id){
-      books.splice(i, 1);
-      break;
+      return i
     }
   }
+}
+
+function removeBook(e){
+  let index = getBookIndex(e);
+  books.splice(index, 1);
   updateCards();
 }
 
@@ -43,6 +50,23 @@ function updateCards(){
     pagesDiv.classList.add("pages");
     pagesDiv.textContent = `Pages: ${book.pages}`;
 
+    let readDiv = document.createElement("div");
+    readDiv.classList.add("read");
+    let readSpan=document.createElement("span");
+    readDiv.appendChild(readSpan);
+    (book.read)
+    ? readSpan.textContent = "Read"
+    : readSpan.textContent = "Unread";
+
+    readSpan.addEventListener("click", (e) => {
+      let index = getBookIndex(e);
+      console.log(index);
+      (books[index].read)
+      ? books[index].read = false
+      : books[index].read = true;
+      updateCards();
+    });
+
     let idDiv = document.createElement("div");
     idDiv.classList.add("id");
     idDiv.textContent = book.id;
@@ -56,6 +80,7 @@ function updateCards(){
     cardDiv.appendChild(titleDiv);
     cardDiv.appendChild(authorDiv);
     cardDiv.appendChild(pagesDiv);
+    cardDiv.appendChild(readDiv);
     cardDiv.appendChild(idDiv);
     cardDiv.appendChild(deleteButton);
   }
@@ -69,10 +94,19 @@ let bookContainerDiv = document.querySelector(".bookContainer");
 let newBookTitle = document.querySelector("#titleInput");
 let newBookAuthor = document.querySelector("#authorInput");
 let newBookPages = document.querySelector("#pagesInput");
+let newBookRead = document.querySelector("#readInput");
 
 let addBookBtn = document.querySelector("#addBookBtn");
 
+let errorDiv = document.querySelector(".error");
+
 addBookBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  addBookToLibrary(newBookTitle.value, newBookAuthor.value, newBookPages.value);
+  if(Boolean(newBookTitle.value) && Boolean(newBookAuthor.value) && Boolean(newBookPages.value)){
+    addBookToLibrary(newBookTitle.value, newBookAuthor.value, newBookPages.value, newBookRead.checked)
+    errorDiv.classList.add("hide");
+  }
+  else{
+    errorDiv.classList.remove("hide");
+  };
 });
